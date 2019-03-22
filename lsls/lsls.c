a5b611a6ade6b391fef6f50d8da646f3a6f573a7
root <root@LAPTOP-0B6F4Q2G.localdomain>
2019-03-22 09:33:39 -0600

    printing all files

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 
 /**
@@ -7,12 +8,42 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  // check if more than two arguments, if so error
+  if (argc > 2)
+  {
+    perror("The signature for the command is lsls [,path]");
+    exit(1);
+  }
+  char *target;
+  // set target to second argument if it exists
+  if (argc == 2)
+  {
+    target = argv[1];
+  }
+  // else check if its only one line, then set target to "."
+  else
+  {
+    target = ".";
+  }
 
   // Open directory
+  DIR *dir;
+  dir = opendir(target);
+  if (!dir)
+  {
+    perror("Failed to open directory");
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  struct dirent *current = readdir(dir);
+  while (current)
+  {
+    printf("%s\n", current->d_name);
+    current = readdir(dir);
+  }
 
-  // Close directory
+  closedir(dir);
 
   return 0;
 }
